09fcfc6eb105c10df247d299b3f9a15d56193995
Clara <mariaclcostabeli@gmail.com>
2026-05-13 15:49:47 -0300

    aula 2 de python

--- 1.1-numeros-em-python.py
+++ 1.1-numeros-em-python.py
@@ -14,7 +14,7 @@ Vamos aprender:
 # float - números com casas decimais
 # complex - números complexos (usado em matemática/engenharia)
 
-print ("===== TIPO NÚMERICOS =====")
+print("===== TIPO NÚMERICOS =====")
 
 # EXEMPLO 01 - NÚMERO INTEIRO
 
@@ -22,12 +22,12 @@ print ("===== TIPO NÚMERICOS =====")
 numero_inteiro = 10
 
 #Mostramos o valor
-print ("Valor:", numero_inteiro)
+print("Valor:", numero_inteiro)
 
 #Type() mostra qual é o tipo da variável
-print ("Tipo:", type(numero_inteiro))
+print ("Type:", type(numero_inteiro))
 
-print (-----------------------------)
+print ("-----------------------------")
 
 # EXEMPLO 02 - NÚMEROS DECIMAL
 
@@ -57,3 +57,40 @@ print ("Valor:", numero_complexo)
 print ("Type:", type(numero_complexo))
 
 print ("-----------------------------")
+
+#EXEMPLO 03 - ACESSO CADA PARTE DO NÚMERO
+
+#.REAL RETORNA A PARTE REAL
+print ("Parte Real:", numero_complexo.real)
+#.imag retorna a parte imaginária
+print ("Parte Imaginaria:", numero_complexo.imag)
+
+#PASSO 02 - CONVERSÃO TIPOS
+# Apenas para separar visualmente a saída no terminal
+print("\n\n")
+##Exemplo Classico:
+## Dadods vindos do usuário são sempre do tipo string, muitas vezes é necessário converter eles
+
+print ("===== conversões =====")
+
+# float - int
+
+valor = int (3.9)
+
+print ("int (3.9):", valor)
+print ("Tipo:", type(valor))
+
+#STRING -> int
+valor1 = "10"
+print(type(valor1))
+print("int (valor1):", int(valor1))
+print("Tipo:", type(int(valor1)))
+
+valor2 = int ("10")
+print('int("10"):', valor2)
+print("tipo", type(valor2))
+
+#int --> Float
+valor3 = float (10)
+print ("float(10):", valor3)
+print ("Tipo:", type(valor3))
