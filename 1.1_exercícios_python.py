138776e021f86281df4daac202df2cb8cabccab8
Clara <mariaclcostabeli@gmail.com>
2026-05-15 10:34:46 -0300

    1.1_exercícios_python.py

---
+++ 1.1_exercícios_python.py
@@ -0,0 +1,66 @@
+# EX1
+# Um aluno tem 10 anos. Armazene essa idade em uma variável
+# e exiba seu tipo.
+idade = 10
+print (type (idade))
+
+
+# EX2
+# A temperatura medida é 23.5°C.
+# Armazene esse valor e mostre seu tipo.
+temperatura = 23.5
+print (type(temperatura))
+
+
+# EX3
+# Crie um número complexo representando uma impedância elétrica
+# de 5 + 8j e mostre sua parte real.
+impedância = 5 +8j
+print(impedância.real)
+
+
+# EX4
+# Mostre a parte imaginária do número complexo
+# criado no exercício anterior.
+print(impedância.imag)
+
+
+# EX5
+# Declare uma variável chamada "populacao"
+# com o valor 8_000_000_000 (8 bilhões)
+# e mostre seu tipo.
+populacao = 8_000_000_000
+print(type(populacao))
+
+
+# EX6
+# Verifique se o número 7 é do tipo int
+# usando a função type().
+print(type(7)) == int
+
+
+# EX7
+# Crie uma variável chamada "aprovado"
+# com o valor booleano True e mostre seu tipo.
+aprovado = True
+print(type(aprovado))
+
+
+# EX8
+# Some True e False e mostre o resultado
+# e também o tipo do resultado.
+resultado = True + False
+print(resultado)
+print(type(resultado))
+
+# EX9
+# Pesquise e mostre qual é o valor máximo
+# que um número inteiro pode ter em Python.
+import sys
+print (sys.maxsize)
+
+
+# EX10
+# Mostre a representação em binário
+# do número 10 usando uma função do Python.
+print(bin(10))
