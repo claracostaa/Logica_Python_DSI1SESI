3884c9d97c6e5dfa00046c5374c3ef522d48f926
clara <mariaclcostabeli@gmail.com>
2026-05-20 10:23:56 -0300

    1.1_conversões_de_tipos.py

--- 1.1_conversões_de_tipos.py
+++ 1.1_conversões_de_tipos.py
@@ -20,7 +20,9 @@ print("Resultado:", valor * 2)
 # EX4
 # Tente converter a string "cento e vinte"
 # para inteiro e observe o que acontece.
-
+valor = int ("cento e vinte")
+valor ("classe:," type (valor))
+#ERROR!!
 
 # EX5
 # Converta o número 42 para string
