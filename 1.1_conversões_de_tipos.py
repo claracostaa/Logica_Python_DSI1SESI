0a5c8892cea1d79debd6cb4adc5d9255feabec8b
clara <mariaclcostabeli@gmail.com>
2026-05-20 10:18:22 -0300

    1.1_conversões_de_tipos.py

---
+++ 1.1_conversões_de_tipos.py
@@ -0,0 +1,59 @@
+# EX1
+# O usuário digitou "25" como sua idade em um formulário.
+# Converta para inteiro e calcule a idade que ele terá
+# daqui a 5 anos.
+idade = int("25")
+print("idade daqui a 5 anos:", idade + 5 )
+
+# EX2
+# Converta o número de ponto flutuante 7.999
+# para inteiro e observe o resultado.
+numero = int(7.999)
+print("Conversão para inteiro:", numero)
+
+# EX3
+# Converta a string "-3.14" para float
+# e multiplique o resultado por 2.
+valor = float("-3.14")
+print("Resultado:", valor * 2)
+
+# EX4
+# Tente converter a string "cento e vinte"
+# para inteiro e observe o que acontece.
+
+
+# EX5
+# Converta o número 42 para string
+# e concatene com a palavra " respostas".
+texto = str ("42") + " respostas"
+print(texto)
+
+# EX6
+# Use a função complex() para criar
+# um número complexo com parte real 3
+# e parte imaginária 5.
+numero_complexo = complex(3, 5)
+print("Número complexo:", numero_complexo)
+
+# EX7
+# Converta o número 0 para booleano
+# e mostre o resultado.
+print(bool(0))
+
+# EX8
+# Converta o número -100 para booleano
+# e mostre o resultado.
+print(bool(-100))
+
+# EX9
+# Converta o número 3.1415 para inteiro
+# e depois para string, tudo em uma única linha.
+resultado = str(int(3.1415))
+print(resultado)
+
+# EX10
+# Some um número inteiro (5) com um float (2.3)
+# e verifique qual é o tipo do resultado.
+soma = 5 + 2.3
+print("Resultado da soma:", soma)
+print("Tipo do resultado:", type(soma))
