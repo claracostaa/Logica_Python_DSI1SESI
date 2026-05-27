f83b59a3493d973ef85a3cced41b1d10248af273
claracostaa <mariaclcostabeli@gmail.com>
2026-05-27 11:58:20 -0300

    2.1_exercios.py

--- Modulo_02/2.1_manipular_texto_em_python.py
+++ Modulo_02/2.1_manipular_texto_em_python.py
@@ -58,3 +58,51 @@ print("Do ínicio até 3:", st[:3])
 print ("Do 2 até o fim:", st[2:])
 
 print ("Tamanho:", len(st))
+
+# 5 - OPERAÇÕES COM STRINGS
+# Python permite várias operações com strings
+
+print("m" in st)
+# Significa que a letra "m" existe dentro da string
+
+print("x" not in st)
+# Significa que "X" não existe na string
+
+print("m" * 3)
+# Multiplicação repete a string
+
+print("m" + "aracana" + texto1)
+# Operador + concatena strings
+
+# 6 - STRINGS SÃO IMUTÁVEIS
+# Strings não podem ser alteradas diretamente!!!
+# Isso siginifica que o conteúdo original não muda.
+# O que acontece é a criação de uma nova strings
+
+texto = "python 3"
+
+# Método replace cria uma nova string
+texto = texto.replace("3", "2")
+print(texto)
+
+# 7 - MÉTODOS IMPORTANTES
+# Strings possuem vários métodos úteis.
+
+cidade = "maracana"
+# Coloca a primeira letra em maiúscula.
+print(cidade.capitalize())
+
+# Contar quantas vezes "a" aparece
+print(cidade.count("a"))
+
+# Verifica se começa com "m"
+print(cidade.startswith("m"))
+
+# Verifica se termina com "z"
+print(cidade.endswith("z"))
+
+frase = "copa de 2002"
+
+print(frase.split(" "))
+
+# 8 - FORMATAÇÃO DE STRINGS
